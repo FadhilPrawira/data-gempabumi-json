609e5605f793f67cb450e9853f1d4f3007415681
Fadhil Prawira <fadhilprawira87@gmail.com>
2021-05-17 14:54:07 +0700

    Create autogempa.py

---
+++ autogempa.py
@@ -0,0 +1,12 @@
+import function
+import json
+result = json.loads(function.autogempa())
+mapURL = result["Shakemap"]
+result.pop("Shakemap")
+print("")
+for x in result:
+    print("{:<15} : ".format(x) + result[x])
+
+print("{:<15} :".format("Shakemap"))
+import IPython
+IPython.display.Image(mapURL)
